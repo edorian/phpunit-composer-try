3617bb5aa6c2cf32288c09387a4264728baf4924
Volker Dusch <github@wallbash.com>
2012-07-06 17:09:28 +0200

    Use composers autoload to set the include pathes like @stof suggested. Thanks :)

--- phpunit-wrapper.php
+++ phpunit-wrapper.php
@@ -1,33 +1,7 @@
 <?php
 
-set_include_path(
-    get_include_path() .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/PHP_CodeCoverage' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/PHPUnit' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/PHPUnit_MockObject' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/PHP_Timer' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/PHP_TokenStream' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/Text_Template' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/File_Iterator' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/PHP_Invoker' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/PHPUnit_Story' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/PHPUnit_Selenium' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.phpunit.de/DbUnit' .
-    PATH_SEPARATOR .
-    __DIR__ . '/../../pear-pear.symfony-project.com/YAML/'
-);
-
+require __DIR__ . '/../../autoload.php';
 require __DIR__ . '/../../pear-pear.phpunit.de/PHPUnit/PHPUnit/Autoload.php';
+
 PHPUnit_TextUI_Command::main();
 
